5b2c2eb1ffe205177150860a3f8eacfc9d90ab2e
刘雪峰 <2241159181@qq.com>
2026-04-16 20:13:07 +0800

    导入router数据集并在用例中进行读取

--- loona_interface_agent_test/test_case_one.py
+++ loona_interface_agent_test/test_case_one.py
@@ -1,19 +1,22 @@
+import logging
 from unittest import TestCase
 
+import allure
 import pytest
 
 from common.excel_reader_util import ExcelReaderUtil
 from common.request_handler import send_request
 from common.yaml_loader import load_yaml_config
+loona_router = ExcelReaderUtil.read_excel("../data/test_data_case/agent_router_415_cases.xlsx")
+column_list_router = [item["真实请求体"] for item in loona_router]
 
-ExcelReaderUtil.read_excel("")
 
-
+allure.epic("agent测试-loona")
 @pytest.mark.parametrize(
-
+    "router_request_body",column_list_router
 )
-class TestCaseOne(TestCase):
-    def test_case_one(self):
-        send_request(
+class TestCaseOne():
 
-        )
+    def test_case_one(self, router_request_body):
+        with allure.step(""):
+            logging.info(f"当前的请求体内容为：{router_request_body}")
