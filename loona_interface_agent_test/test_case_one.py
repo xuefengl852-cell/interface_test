252adcd3b410be5209de0faf72d21c51b2071d39
刘雪峰 <2241159181@qq.com>
2026-04-16 19:37:43 +0800

    修改测试框架版本结构

---
+++ loona_interface_agent_test/test_case_one.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+import pytest
+
+from common.excel_reader_util import ExcelReaderUtil
+from common.request_handler import send_request
+from common.yaml_loader import load_yaml_config
+
+ExcelReaderUtil.read_excel("")
+
+
+@pytest.mark.parametrize(
+
+)
+class TestCaseOne(TestCase):
+    def test_case_one(self):
+        send_request(
+
+        )
